5f769a6a417423e1ce9fb95470ad26bb5249cbd6
BhuvaaneshR <bhuvaaneshrajeshwarantvl@gmail.com>
2026-06-17 20:39:15 +0530

    capitalize(), title(), strip(), upper(), lower()

---
+++ strings-methods.py
@@ -0,0 +1,42 @@
+#.capitalize() method - Converts first character to uppercase and remaining characters to lowercase.
+"""
+text1 = "hELLO WORLD"
+text1 = text1.capitalize()
+print (text1)
+"""
+
+
+
+#.title() method - Capitalizes first letter of every word.
+"""
+text1 = "hELLO WORLD"
+text1 = text1.title()
+print (text1)
+"""
+
+
+
+#.strip() method - removes whitespace across starting and ending of a string.
+"""
+text1 = "hELLO WORLD                   "
+text1 = text1.strip()
+print (text1)
+"""
+
+
+
+#.upper() method - converts all characters to uppercase.
+"""
+text1 = "HelloWorld"
+text1 = text1.upper()
+print (text1)
+"""
+
+
+
+#.lower() method - converts all characters to lowercase.
+"""
+text1 = "HelloWorld"
+text1 = text1.lower()
+print (text1)
+"""
